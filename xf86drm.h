86599409840b01c3bd454b0d4d0477bc631b09f9
Aurélien Larcher <aurelien.larcher@gmail.com>
2018-11-06 17:08:44 +0100

    xf86drm.h

--- xf86drm.h
+++ xf86drm.h
@@ -472,6 +472,17 @@ do {	register unsigned int __old __asm("o0");		\
 #endif /* architecture */
 #endif /* __GNUC__ >= 2 */
 
+#if defined(__SUNPRO_C)
+#include <atomic.h>
+#define atomic_cmpset_int(p, c, n) ((c == atomic_cas_uint(p, c, n)) ? 1 : 0)
+#define DRM_CAS(lock,old,new,__ret)          \
+               do {                          \
+                                       unsigned int __result, __old = (old);\
+                                       __result = !atomic_cmpset_int(lock,__old,new);\
+                                       __ret = __result;          \
+                               } while(0)
+#endif
+
 #ifndef DRM_CAS
 #define DRM_CAS(lock,old,new,ret) do { ret=1; } while (0) /* FAST LOCK FAILS */
 #endif
@@ -843,6 +854,14 @@ extern void drmFreeDevices(drmDevicePtr devices[], int count);
 #define DRM_DEVICE_GET_PCI_REVISION (1 << 0)
 extern int drmGetDevice2(int fd, uint32_t flags, drmDevicePtr *device);
 extern int drmGetDevices2(uint32_t flags, drmDevicePtr devices[], int max_devices);
+
+#ifdef __sun
+extern int _sun_drm_get_major(void);
+extern int _sun_drm_find_device(int min, char **pathp);
+extern int _sun_drm_get_subsystem(char *path);
+extern int _sun_drm_get_pci_bus_info(char *path, drmPciBusInfo *bus);
+extern int _sun_drm_get_pci_dev_info(char *path, drmPciDeviceInfo *dev);
+#endif /* __sun */
 
 #if defined(__cplusplus)
 }
